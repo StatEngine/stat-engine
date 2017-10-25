be78854a0c71a746f85a0ad889c7bfb8eb43bdf7
Joe Chop <joe.chop@prominentedge.com>
2017-10-25 12:10:21 -1000

    resovle warnings

--- server/routes.js
+++ server/routes.js
@@ -27,7 +27,6 @@ export default function(app) {
   // All other routes should redirect to the index.html
   app.route('/*')
     .get((req, res) => {
-      res.send('/');
       res.sendFile(path.resolve(`${app.get('appPath')}/index.html`));
     });
 }
