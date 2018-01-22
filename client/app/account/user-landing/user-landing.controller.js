87a507e6b07640659a363251d71d4f8aad359fd8
bingles <brian.ingles@gmail.com>
2018-01-22 12:57:42 -0500

    Adds additional user landing stylign and infreastructure #61

--- client/app/account/user-landing/user-landing.controller.js
+++ client/app/account/user-landing/user-landing.controller.js
@@ -1,12 +1,5 @@
 'use strict';
 
 export default class UserLandingController {
-  /*@ngInject*/
-  constructor(Modal) {
-    this.Modal = Modal;
-  }
-
-  openModal() {
-    this.Modal.confirm.delete(() => {})();
-  }
+  
 }
