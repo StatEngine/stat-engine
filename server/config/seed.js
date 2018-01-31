bbff950fe1906afb222980e5b620ad1aab7876bc
Joe Chop <joe.chop@prominentedge.com>
2018-01-31 11:59:50 -1000

    kibana access

--- server/config/seed.js
+++ server/config/seed.js
@@ -25,7 +25,7 @@ User
     timezone: 'US/Eastern',
     Users: [{
       provider: 'local',
-      role: 'user,kibana_ro',
+      role: 'user,kibana_admin',
       username: 'richmond',
       first_name: 'Richmond',
       last_name: 'User',
--- server/kibana/index.js
+++ server/kibana/index.js
@@ -8,6 +8,10 @@ const proxy = require('http-proxy-middleware');
 
 const router = new Router();
 
-router.use('*', auth.isAuthenticated, auth.hasRole('kibana_ro_strict'), auth.hasFireDepartment, proxy(settings));
+router.use('*',
+  auth.isAuthenticated,
+  auth.hasRole('kibana_admin'), 
+  auth.hasFireDepartment,
+  proxy(settings));
 
 module.exports = router;
