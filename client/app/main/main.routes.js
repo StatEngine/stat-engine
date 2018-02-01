913ecdfd8f9047835f0c63e6d65b6045ac97a7de
Joe Chop <joe.chop@prominentedge.com>
2018-01-31 14:59:41 -1000

    resolve #25 add company info

--- client/app/main/main.routes.js
+++ client/app/main/main.routes.js
@@ -8,24 +8,28 @@ export default function routes($stateProvider) {
       abstract: true,
       template: '<div ui-view />'
     })
-    .state('site.main.main', {
-      url: '/main',
-      component: 'main',
-    })
-    .state('site.main.termsOfUse', {
-      url: '/termsOfUse',
-      template: require('./terms-of-use/terms-of-use.html'),
+    .state('site.main.about', {
+      url: '/about',
+      template: require('./about/about.html'),
     })
     .state('site.main.funding', {
       url: '/funding',
       template: require('./funding/funding.html'),
     })
-    .state('site.main.partners', {
-      url: '/partners',
-      template: require('./partners/partners.html'),
+    .state('site.main.main', {
+      url: '/main',
+      component: 'main',
     })
     .state('site.main.maintenance', {
       url: '/maintenance',
       template: require('./maintenance/maintenance.html'),
+    })
+    .state('site.main.termsOfUse', {
+      url: '/termsOfUse',
+      template: require('./terms-of-use/terms-of-use.html'),
+    })
+    .state('site.main.partners', {
+      url: '/partners',
+      template: require('./partners/partners.html'),
     });
 }
