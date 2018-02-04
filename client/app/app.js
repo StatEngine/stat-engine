6fc6cc2dc0902e63b5d3833399d8e11f6f3ad3b0
Joe Chop <joe.chop@prominentedge.com>
2018-02-03 21:56:01 -1000

    google fonts

--- client/app/app.js
+++ client/app/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 import angular from 'angular';
-// import ngAnimate from 'angular-animate';
+import ngAnimate from 'angular-animate';
 import ngCookies from 'angular-cookies';
 import ngResource from 'angular-resource';
 import ngSanitize from 'angular-sanitize';
@@ -12,6 +12,7 @@ import uiBootstrap from 'angular-ui-bootstrap';
 // import ngMessages from 'angular-messages';
 import ngValidationMatch from 'angular-validation-match';
 
+import angularLoadingBar from 'angular-loading-bar';
 import {
   routeConfig
 } from './app.config';
@@ -38,15 +39,20 @@ import util from '../components/util/util.module';
 
 import './app.scss';
 
-angular.module('statEngineApp', [ngCookies, ngResource, ngSanitize, ngValidationMatch, /*'btford.socket-io',*/ uiRouter,
-  uiBootstrap, _Auth, account, admin, api, guides, navbar, spade, user, modal, footer, main, constants, /*socket,*/ util
+angular.module('statEngineApp', [ngCookies, ngResource, ngSanitize, ngValidationMatch, ngAnimate, /*'btford.socket-io',*/ uiRouter,
+  uiBootstrap, 'angular-loading-bar', _Auth, account, admin, api, guides, navbar, spade, user, modal, footer, main, constants, /*socket,*/ util,
 ])
   .config(routeConfig)
   .run(function($transitions) {
     'ngInject';
 
     $transitions.onSuccess({}, () => $('html, body').animate({ scrollTop: 0 }, 200));
-  });
+  })
+  .config(['cfpLoadingBarProvider', function(cfpLoadingBarProvider) {
+    cfpLoadingBarProvider.latencyThreshold = 100;
+  }])
+
+
 
 angular.element(document)
   .ready(() => {
