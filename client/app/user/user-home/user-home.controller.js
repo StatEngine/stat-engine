7f4099788e6789a44cac42073986a64352ef588c
Tyler Garner <garnertb@gmail.com>
2018-02-09 04:56:26 -0500

    Reword getting started steps.  Make email clickable.

--- client/app/user/user-home/user-home.controller.js
+++ client/app/user/user-home/user-home.controller.js
@@ -21,21 +21,17 @@ export default class UserHomeController {
       //   status: _.isNumber(this.principal.fire_department__id),
       //   action: 'What department are you with?'
       // },
-      { step: 'Create an Account',
-        status: !_.isEmpty(this.principal),
-        action: 'Sign up!'
-      },
-      { step: 'Data Integration',
+      { step: 'Connect your data.',
         status: _.get(this.fireDepartment, 'integration_complete', false),
-        action: 'Contact our integration team at contact@statengine.io to learn how to integrate your data into StatEngine.'
+        action: 'Contact our integration team at <a id=\'user-home-getting-started-connect-data-email\' target="_blank" href="mailto:contact@statengine.io">contact@statengine.io</a> to learn how to integrate your data into StatEngine.'
       },
-      { step: 'Dashboard Access',
-        status: this.principal.roles.indexOf('kibana_admin') > -1,
-        action: 'After your data integration is complete, we\'ll create a custom dashboard just for you!'
-      },
-      { step: 'Verify the Integration',
+      { step: 'Verify your data.',
         status: _.get(this.fireDepartment, 'integration_verified', false),
         action: 'Visit your dashboard and confirm that everything looks good!'
+      },
+      { step: 'Access your dashboard.',
+        status: this.principal.roles.indexOf('kibana_admin') > -1,
+        action: 'After your data integration is complete, we\'ll create a custom dashboard just for you!'
       },
     ];
 
