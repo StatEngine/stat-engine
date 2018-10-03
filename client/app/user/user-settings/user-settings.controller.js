8f5e15d17790b5bc430ac7257ae688d492e6f374
Joe Chop <joe.chop@prominentedge.com>
2018-10-03 11:21:46 -1000

    fix passwor reset

--- client/app/user/user-settings/user-settings.controller.js
+++ client/app/user/user-settings/user-settings.controller.js
@@ -8,9 +8,7 @@ export default class UserSettingsController {
     last_name: '',
     error: ''
   };
-  errors = {
-    error: undefined
-  };
+  passwordErrors = [];
   submitted = false;
 
   /*@ngInject*/
@@ -19,10 +17,12 @@ export default class UserSettingsController {
     this.UserService = User;
     this.$state = $state;
     this.saved = false;
+    this.savedPassword = false;
   }
 
   $onInit() {
     this.profileForm = $('#profileForm').parsley();
+    this.passwordForm = $('#passwordForm').parsley();
   }
 
   edituser(form) {
@@ -38,14 +38,39 @@ export default class UserSettingsController {
         username: this.user.username,
       }).$promise
         .then(() => {
+          this.errors = {};
           this.saved = true;
           form.$setPristine(true);
-          console.dir(form);
         })
         .catch(err => {
-          console.dir(err)
           if(err.data.error) this.errors.error = err.data.error;
           else this.errors.error = 'Error saving data.';
+        });
+    }
+  }
+
+  changePassword(form) {
+    this.submitted = true;
+
+    this.savedPassword = false;
+
+    if(this.passwordForm.isValid()) {
+      this.passwordErrors = [];
+      this.UserService.changePassword({ id: this.user._id }, { username: this.user.username, oldPassword: this.user.oldPassword, newPassword: this.user.newPassword }).$promise
+        .then(() => {
+          // Logged in, redirect to user home
+          this.savedPassword = true;
+          form.$setPristine(true);
+        })
+        .catch(err => {
+          if(err.data.password) {
+            this.passwordErrors.push({ message: err.data.password });
+          } else if(err.data.error) {
+            this.passwordErrors.push({ message: err.data.error });
+          } else {
+            this.passwordErrors.push({ message: 'Error saving data.' });
+          }
+          console.dir(this.passwordErrors)
         });
     }
   }
