d3e7d585a52c56e81814c0608c9da7f69ef1a968
Tyler Garner <garnertb@gmail.com>
2018-12-07 16:13:39 -0500

    Add logic to prevent creating existing customers.

--- server/subscription/chargebee.js
+++ server/subscription/chargebee.js
@@ -6,17 +6,31 @@ chargebee.configure({
   api_key: process.env.CHARGEBEE_API_KEY});
 
 
-export const createCustomer = fireDepartment => {
-  if(_.isEmpty(process.env.CHARGEBEE_API_KEY)) {
+export const createCustomer = async fireDepartment => {
+  let skip = _.isEmpty(process.env.CHARGEBEE_API_KEY) || _.isEmpty(fireDepartment);
+
+  //if customer id check
+  if(!skip && !_.isEmpty(fireDepartment.customer_id)) {
+    const customer = await chargebee.customer
+      .retrieve(fireDepartment.customer_id)
+      .request()
+      .catch(console.log);
+
+    if(customer) {
+      // this customer already exists in chargebee.
+      skip = true;
+    }
+  }
+
+  if(skip) {
+    // one of the check's failed skip
     return;
   }
 
-  return chargebee.customer.create({ company: fireDepartment.name.substring(0, 250) })
-    .request()
-    .then(response => {
-      fireDepartment.customer_id = response.customer.id;
-      return fireDepartment.save();
-    });
+  let customerResponse = await chargebee.customer
+    .create({ company: fireDepartment.name.substring(0, 250) }).request();
+  fireDepartment.customer_id = customerResponse.customer.id;
+  fireDepartment.save();
 };
 
 
