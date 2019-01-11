9141bd7e872d57eeda9a8ba6860b7e6633ce2400
Joe Chop <joe.chop@prominentedge.com>
2019-01-11 13:47:11 -1000

    fix incident analysis page

--- server/api/incident/incident.controller.js
+++ server/api/incident/incident.controller.js
@@ -274,7 +274,7 @@ export function loadComparison(req, res, next) {
   let neighborhood = _.get(req.incident, 'address.location.neighborhood');
 
   const pulsePoint = !_.isEmpty(req.incident.description.extended_data.AgencyIncidentCallTypeDescription);
-  const incidentType = pulsePoint ? req.incident.description.extended_data.AgencyIncidentCallTypeDescription : req.incident.description.type;
+  const incidentType = pulsePoint ? _.get(req.incident, 'description.extended_data.AgencyIncidentCallTypeDescription') : req.incident.description.type;
   const incidentTypeFilter = pulsePoint ? { term: {'description.extended_data.AgencyIncidentCallTypeDescription.keyword': incidentType }} : { term: {'description.type': incidentType }};
 
   const compValues = {};
