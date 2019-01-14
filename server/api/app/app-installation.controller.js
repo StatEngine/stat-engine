837b9621e8168028b3eb294b181fc54d499e9d79
Joe Chop <joe.chop@prominentedge.com>
2019-01-14 08:45:25 -1000

    wip

--- server/api/app/app-installation.controller.js
+++ server/api/app/app-installation.controller.js
@@ -1,6 +1,5 @@
 import _ from 'lodash';
 import jwt from 'jsonwebtoken';
-import config from '../../config/environment';
 
 import {
   App,
@@ -9,41 +8,19 @@ import {
 } from '../../sqldb';
 
 export function search(req, res) {
-  if(_.isEmpty(req.user.client_id)) return res.send(404);
-
   return AppInstallation.findAll({
     include: [{
       model: App,
-      attributes: [
-        '_id',
-        'name',
-        'slug',
-      ],
       where: { client_id: req.user.client_id }
     }, {
       model: FireDepartment,
-      attributes: [
-        '_id',
-        'fd_id',
-        'name',
-        'state',
-        'firecares_id',
-        'timezone',
-      ]
     }]
   })
-    .then(appInstallations => res.json(appInstallations))
-    .catch(e => {
-      console.error(e);
-      res.sendStatus(500);
-    })
+    .then(appInstallations => res.json(appInstallations));
 }
 
-const EXPIRES_IN = 60 * 60;
+const expires_in = 60*60;
 export function generateToken(req, res) {
-  if(_.isEmpty(req.user.client_id)) return res.send(404);
-  if(_.isEmpty(req.params.installationId)) return res.send(404);
-
   AppInstallation.find({
     where: {
       _id: req.params.installationId
@@ -53,58 +30,24 @@ export function generateToken(req, res) {
       where: { client_id: req.user.client_id }
     }, {
       model: FireDepartment,
-      attributes: [
-        '_id',
-        'fd_id',
-        'name',
-        'state',
-        'firecares_id',
-        'timezone',
-      ]
     }]
   })
-    .then(installation => {
-      if(_.isNil(installation)) return res.sendStatus(404);
-      return res.json({
-        token_type: 'Bearer',
-        access_token: jwt.sign({
-          roles: ['app'],
-          permissions: installation.App.permissions || [],
-          FireDepartment: installation.FireDepartment.get(),
-        }, config.oauth.secret, { expiresIn: EXPIRES_IN }),
-        expires_in: EXPIRES_IN
-      })
-    })
-    .catch(e => {
-      console.error(e);
-      res.sendStatus(500);
-    })
+    .then(installation => res.json({
+      token_type: 'Bearer',
+      access_token: jwt.sign({ roles: ['user'], FireDepartment: installation.FireDepartment.get() }, 'top_secret', { expiresIn: expires_in }),
+      expires_in
+    }))
 }
 
 export function get(req, res) {
-  if(_.isEmpty(req.user.client_id)) return res.send(404);
-  if(_.isEmpty(req.params.installationId)) return res.send(404);
-
   AppInstallation.find({
     where: {
       _id: req.params.installationId
-
     },
     include: [{
       model: App,
-      where: { client_id: req.user.client_id },
-      attributes: [
-        '_id',
-        'slug',
-      ],
+      where: { client_id: req.user.client_id }
     }]
   })
-    .then(installation => {
-      if(_.isNil(installation)) return res.sendStatus(404);
-      return res.json(installation)
-    })
-    .catch(e => {
-      console.error(e);
-      res.sendStatus(500);
-    });
+    .then(installation => res.json(installation));
 }
--- server/api/app/index.js
+++ server/api/app/index.js
@@ -3,39 +3,32 @@
 import { Router } from 'express';
 
 import * as auth from '../../auth/auth.service';
-import jwt from 'jsonwebtoken';
+import * as appInstallationController from './app-installation.controller';
 
 const router = new Router();
 
-const hello = (req, res) => {
-  res.send('Hello')
-};
-
-const token = (req, res) => {
-  const body = { _id : '123' };
-
-  // Sign the JWT token and populate the payload with the user email and id
-  const token = jwt.sign({ user : body }, 'top_secret');
-  // Send back the token to the user
-  return res.json({ token });
-};
 
 router.get(
-  '/installations/access_tokens',
+  '/installations/:installationId/access_tokens',
   auth.checkOauthJwt,
-  token,
+  appInstallationController.generateToken,
+);
+
+router.get(
+  '/installations/:installationId/',
+  auth.checkOauthJwt,
+  appInstallationController.get,
 );
 
 router.get(
   '/installations',
   auth.checkOauthJwt,
-  hello,
+  appInstallationController.search,
 );
 
 router.get(
   '/',
   auth.checkOauthJwt,
-  hello,
 );
 
 module.exports = router;
