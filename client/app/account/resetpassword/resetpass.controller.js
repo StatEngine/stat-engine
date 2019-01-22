b810dabce2b506f4a32064141e7796ba18f3acbe
Lars Kendall <lars@prominentedge.com>
2019-01-22 08:50:52 -0800

    Reset password alert. (#140)
    * Show an alert after password reset email is sent.

--- client/app/account/resetpassword/resetpass.controller.js
+++ client/app/account/resetpassword/resetpass.controller.js
@@ -10,9 +10,10 @@ export default class ResetPasswordController {
   submitted = false;
 
   /*@ngInject*/
-  constructor(User, $state) {
+  constructor(User, $state, Modal) {
     this.UserService = User;
     this.$state = $state;
+    this.Modal = Modal;
   }
 
   /**
@@ -23,7 +24,15 @@ export default class ResetPasswordController {
     if(form.$valid) {
       this.UserService.resetPassword({}, { useremail: this.user.email }).$promise
         .then(() => {
-          this.$state.go('site.account.login');
+          this.Modal.custom({
+            title: 'Email Sent',
+            content: "An email was sent with instructions to reset your password. Remember to check your spam if you don't see the email after a while.",
+            buttons: [{
+              text: 'Sign In',
+              style: this.Modal.buttonStyle.primary,
+              onClick: () => this.$state.go('site.account.login'),
+            }],
+          }).present();
         })
         .catch(err => {
           if(err.data.error) this.errors.email = err.data.error;
