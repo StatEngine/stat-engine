fc3cb35245b236f21b8c6a5d22d3d597a4ef1e42
Tyler Garner <garnertb@gmail.com>
2019-02-01 09:30:19 -0500

    Reference manage subscription section in the manage subscription alert modal.

--- client/components/auth/router.decorator.js
+++ client/components/auth/router.decorator.js
@@ -23,7 +23,7 @@ export default function routerDecorator($transitions, $rootScope, $cookies, $win
       content: `
         Please renew your subscription. You have <strong>${daysRemainingText}</strong> remaining before your service will be suspended.<br/>
         <br/>
-        To renew your subscription, please contact us at <a href="mailto:contact@statengine.io">contact@statengine.io</a> or by using the chat bubble (<i class="fa fa-comments"></i>) in the lower right corner.
+        To renew your subscription, click "Manage Subscription" on the menu or contact us at <a href="mailto:contact@statengine.io">contact@statengine.io</a> or by using the chat bubble (<i class="fa fa-comments"></i>) in the lower right corner.
       `,
       showCloseButton: false,
       enableBackdropDismiss: false,
@@ -37,7 +37,7 @@ export default function routerDecorator($transitions, $rootScope, $cookies, $win
       content = `
         Your subscription renewal grace period has elapsed and service has been suspended.<br/>
         <br/>
-        To renew your subscription, please contact us at <a href="mailto:contact@statengine.io">contact@statengine.io</a> or by using the chat bubble (<i class="fa fa-comments"></i>) in the lower right corner.
+        To renew your subscription, click "Manage Subscription" on the menu or contact us at <a href="mailto:contact@statengine.io">contact@statengine.io</a> or by using the chat bubble (<i class="fa fa-comments"></i>) in the lower right corner.
       `
     } else {
       content = "Your department's subscription has expired and service has been suspended. Please contact your department administrator to restore service.";
