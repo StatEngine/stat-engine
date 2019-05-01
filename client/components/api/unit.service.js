b3536a3b5e4dfe38ed4e87e0fa80ba1a9186479f
Lars Kendall <lars@prominentedge.com>
2019-04-30 17:30:40 -0700

    Fix inconsistent number of incidents in unit timeline.

---
+++ client/components/api/unit.service.js
@@ -0,0 +1,29 @@
+'use strict';
+
+export default function UnitResource($resource) {
+  'ngInject';
+
+  return $resource('/api/units/:id/:resource/:resource2', {
+    id: '@_id'
+  }, {
+    getResponses: {
+      method: 'GET',
+      params: {
+        resource: 'responses',
+      },
+    },
+    getMetrics: {
+      method: 'GET',
+      params: {
+        resource: 'metrics',
+      },
+    },
+    getMetricsTotal: {
+      method: 'GET',
+      params: {
+        resource: 'metrics',
+        resource2: 'total',
+      },
+    },
+  });
+}
