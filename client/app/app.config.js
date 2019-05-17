caf59fb996bd702fa27de78f4e941792febb379a
Lars Kendall <lars@prominentedge.com>
2019-05-16 18:45:16 -0700

    Fix mobile body size on orientation change. (#271)

--- client/app/app.config.js
+++ client/app/app.config.js
@@ -35,6 +35,23 @@ export function routeConfig($urlRouterProvider, $locationProvider, $stateProvide
   // Just manually set the body height to match the window height, which will account for the extra chrome.
   fitBodyToWindow();
   angular.element(window).on('resize', fitBodyToWindow);
+  angular.element(window).on('orientationchange', async () => {
+    // Mobile devices will change their innerHeight at an undetermined time after the orientation change.
+    // So wait up to 120 frames for a change to the window innerHeight, and then refit the body to the window.
+    await new Promise(resolve => {
+      const checkInnerHeightChanged = (framesWaited, prevHeight) => {
+        if(window.innerHeight !== prevHeight || framesWaited >= 120) {
+          resolve()
+        } else {
+          window.requestAnimationFrame(() => checkInnerHeightChanged(framesWaited + 1, prevHeight));
+        }
+      };
+      checkInnerHeightChanged(0, window.innerHeight);
+    });
+
+    fitBodyToWindow();
+  });
+
   function fitBodyToWindow() {
     angular.element('body').css({
       height: `${window.innerHeight}px`,
