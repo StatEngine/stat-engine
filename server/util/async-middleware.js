ef9fc082ac5936a28cb923fc1f9ee95e89619b62
Lars Kendall <lars@prominentedge.com>
2019-10-09 16:23:44 -0700

    Update comments.

--- server/util/async-middleware.js
+++ server/util/async-middleware.js
@@ -1,9 +1,9 @@
 'use strict';
 
 export function asyncMiddleware(middleware) {
-  // Wrap the middleware with a try/catch block. This will deal with exceptions thrown in middleware that
-  // use async/await. Without this, the server won't send an error to the client if one occurs within
-  // async middleware, causing the client to hang if an uncaught exception occurs on the server.
+  // Wrap the middleware function with a try/catch block. This will deal with exceptions thrown in async function calls.
+  // Without this, the server won't send an error to the client if one occurs within an async function call, causing
+  // the client to hang if an uncaught exception occurs on the server.
   return async (req, res, next) => {
     try {
       await middleware(req, res, next);
--- server/util/async-param.js
+++ server/util/async-param.js
@@ -1,6 +1,9 @@
 'use strict';
 
 export function asyncParam(paramFunc) {
+  // Wrap the param function with a try/catch block. This will deal with exceptions thrown in async function calls.
+  // Without this, the server won't send an error to the client if one occurs within an async function call, causing
+  // the client to hang if an uncaught exception occurs on the server.
   return async (req, res, next, id) => {
     try {
       await paramFunc(req, res, next, id);
