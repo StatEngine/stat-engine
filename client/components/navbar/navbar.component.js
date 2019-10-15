fa0a0debf8d217371278d80c84751c24c9d73401
Lars Kendall <lars@prominentedge.com>
2019-10-15 14:47:14 -0700

    Cleaner solution for navbar dropdowns.

--- client/components/navbar/navbar.component.js
+++ client/components/navbar/navbar.component.js
@@ -5,10 +5,7 @@
 import angular from 'angular';
 
 export class NavbarComponent {
-  isProductsDropdownOpen = false;
-  isCompanyDropdownOpen = false;
-  isAppsDropdownOpen = false;
-  isUserDropdownOpen = false;
+  openDropdowns = {};
 
   constructor($state, $scope, $window, $timeout, Principal, AmplitudeService, AnalyticEventNames, appConfig) {
     'ngInject';
@@ -45,9 +42,7 @@ export class NavbarComponent {
       this.$window.location.href = '/workspace';
     };
 
-    this.goto = function(state, appName) {
-      this.userDropDownActive = false;
-
+    this.goto = (state, appName) => {
       if(appName) {
         this.AmplitudeService.track(this.AnalyticEventNames.APP_ACCESS, {
           app: appName,
@@ -81,81 +76,24 @@ export class NavbarComponent {
     angular.element('body').removeClass('show-mobile-nav');
   }
 
-  showProductsDropdown() {
-    if (this.isProductsDropdownOpen) {
+  openDropdown(dropdownId) {
+    if (this.openDropdowns[dropdownId]) {
       return;
     }
 
-    this.isProductsDropdownOpen = true;
+    this.openDropdowns[dropdownId] = {
+      close: () => {
+        this.$window.removeEventListener('click', this.openDropdowns[dropdownId].close);
+        this.$timeout(() => {
+          delete this.openDropdowns[dropdownId];
+        });
+      },
+    };
 
     this.$timeout(() => {
-      this.$window.addEventListener('click', this.hideProductsDropdown);
+      this.$window.addEventListener('click', this.openDropdowns[dropdownId].close);
     });
   }
-
-  hideProductsDropdown = () => {
-    this.$window.removeEventListener('click', this.hideProductsDropdown);
-    this.$timeout(() => {
-      this.isProductsDropdownOpen = false;
-    });
-  };
-
-  showCompanyDropdown() {
-    if (this.isCompanyDropdownOpen) {
-      return;
-    }
-
-    this.isCompanyDropdownOpen = true;
-
-    this.$timeout(() => {
-      this.$window.addEventListener('click', this.hideCompanyDropdown);
-    });
-  }
-
-  hideCompanyDropdown = () => {
-    this.$window.removeEventListener('click', this.hideCompanyDropdown);
-    this.$timeout(() => {
-      this.isCompanyDropdownOpen = false;
-    });
-  };
-
-  showAppsDropdown() {
-    if (this.isAppsDropdownOpen) {
-      return;
-    }
-
-    this.isAppsDropdownOpen = true;
-
-    this.$timeout(() => {
-      this.$window.addEventListener('click', this.hideAppsDropdown);
-    });
-  }
-
-  hideAppsDropdown = () => {
-    this.$window.removeEventListener('click', this.hideAppsDropdown);
-    this.$timeout(() => {
-      this.isAppsDropdownOpen = false;
-    });
-  };
-
-  showUserDropdown() {
-    if (this.isUserDropdownOpen) {
-      return;
-    }
-
-    this.isUserDropdownOpen = true;
-
-    this.$timeout(() => {
-      this.$window.addEventListener('click', this.hideUserDropdown);
-    });
-  }
-
-  hideUserDropdown = () => {
-    this.$window.removeEventListener('click', this.hideUserDropdown);
-    this.$timeout(() => {
-      this.isUserDropdownOpen = false;
-    });
-  };
 }
 
 export default angular.module('directives.nav', [])
