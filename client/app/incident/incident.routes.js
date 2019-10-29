88867f7c3903a1d2543d28190f36fe8b66ae979c
Lars Kendall <lars@prominentedge.com>
2019-10-29 16:48:39 -0700

    Fix incidents white page on first load. (#401)

--- client/app/incident/incident.routes.js
+++ client/app/incident/incident.routes.js
@@ -49,6 +49,13 @@ export default function routes($stateProvider) {
             import(/* webpackChunkName: "ui-grid" */ 'angular-ui-grid/ui-grid').then(() => $ocLazyLoad.inject('ui.grid')),
           ]);
         },
+        async modules() {
+          const modules = {};
+          modules._ = await import(/* webpackChunkName: "lodash" */ 'lodash');
+          modules.tippy = (await import(/* webpackChunkName: "tippy" */ 'tippy.js')).default;
+          modules.PlotlyBasic = await import(/* webpackChunkName: "plotly-basic" */ 'plotly.js/dist/plotly-basic.js');
+          return modules;
+        },
         currentPrincipal(Principal) {
           return Principal.identity();
         },
