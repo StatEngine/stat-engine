9b87aab1bb153d7e460759403c71fb2c0cc4275a
Luca DeCaprio <luca.decaprio@prominentedge.com>
2020-04-21 19:54:59 -0400

    Started implementing move-up model design

--- client/app/moveup/moveup-home/moveup-home.controller.js
+++ client/app/moveup/moveup-home/moveup-home.controller.js
@@ -2,9 +2,18 @@
 
 'use strict';
 
+const LOCALSTORAGE_DISMISS_KEY = 'move-up-info';
+
 export default class MoveupHomeController {
   /*@ngInject*/
 
   constructor() {
+    this.outputDisabled = true;
+    this.coverageTime = 4;
+    this.dismissed = !!localStorage.getItem(LOCALSTORAGE_DISMISS_KEY)
+  }
+
+  dismiss() {
+    localStorage.setItem(LOCALSTORAGE_DISMISS_KEY, true);
   }
 }
