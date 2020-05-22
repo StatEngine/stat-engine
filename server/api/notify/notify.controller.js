caf7c67b772b81323ec1f7054f6027415d80cc81
Luca DeCaprio <lucamatthewdecaprio@gmail.com>
2020-05-22 15:57:13 -0400

    Snappy Notifications (#472)
    * Removed await and let the notification do its own thing. If push fails, it should fallback to email.
    
    * Added logs and changed return to 204
    
    Co-authored-by: Luca DeCaprio <luca.decaprio@prominentedge.com>

--- server/api/notify/notify.controller.js
+++ server/api/notify/notify.controller.js
@@ -6,18 +6,16 @@ export async function notify(req, res) {
   try {    
     const token = req.NFORS_TOKEN;
 
-    const response = await axios({
+    axios({
       method: 'POST',
       url: process.env.NFORS_API_URL + 'api/notify',
       data: { ...req.body },
       headers: { 'Authorization': `Bearer ${token}` }
-    });
+    })
+    .then(() => console.info('Notification successful'))
+    .catch(() => console.info('Notification failed'));
 
-    if (response.status === 200) {
-      return res.send(200);
-    } else {
-      return res.send(response && response.status || 500);
-    }
+    return res.send(204);
   } catch(err) {
     throw new InternalServerError(err.message);
   }
