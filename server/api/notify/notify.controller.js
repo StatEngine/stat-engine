0b15d695df7d2f07d341000b50bd9893f071c0cc
Luca DeCaprio <luca.decaprio@prominentedge.com>
2020-05-22 15:19:36 -0400

    Removed await and let the notification do its own thing. If push fails, it should fallback to email.

--- server/api/notify/notify.controller.js
+++ server/api/notify/notify.controller.js
@@ -6,18 +6,14 @@ export async function notify(req, res) {
   try {    
     const token = req.NFORS_TOKEN;
 
-    const response = await axios({
+    axios({
       method: 'POST',
       url: process.env.NFORS_API_URL + 'api/notify',
       data: { ...req.body },
       headers: { 'Authorization': `Bearer ${token}` }
     });
 
-    if (response.status === 200) {
-      return res.send(200);
-    } else {
-      return res.send(response && response.status || 500);
-    }
+    return res.send(200);
   } catch(err) {
     throw new InternalServerError(err.message);
   }
