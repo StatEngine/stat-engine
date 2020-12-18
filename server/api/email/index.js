8ab32d807c55b16dfae7a73b66912b88f63c7bbf
Peter Frollo <peter.frollo@prominentedge.com>
2020-12-18 12:15:44 -0600

    added a new endpoint as a copy of the original endpoint

--- server/api/email/index.js
+++ server/api/email/index.js
@@ -1,10 +1,9 @@
-'use strict';
-
 import { Router } from 'express';
 import bodyParser from 'body-parser';
 
 import * as auth from '../../auth/auth.service';
 import * as controller from './email.controller';
+import sendTimeRangeAnalysis from './sendTimeRangeAnalysis';
 
 const router = new Router();
 
@@ -15,6 +14,15 @@ router.post(
   bodyParser.json(),
   auth.hasFireDepartment,
   controller.sendTimeRangeAnalysis,
+);
+
+router.post(
+  '/v2/timeRangeAnalysis',
+  auth.isApiAuthenticated,
+  auth.hasRole('department_admin'),
+  bodyParser.json(),
+  auth.hasFireDepartment,
+  sendTimeRangeAnalysis,
 );
 
 module.exports = router;
