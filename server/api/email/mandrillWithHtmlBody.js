74e7bfef025423f0f53b663a9cb4f6390ad53641
Peter Frollo <peter.frollo@prominentedge.com>
2020-12-18 14:07:17 -0600

    added handbrakes for merging notification data with template

---
+++ server/api/email/mandrillWithHtmlBody.js
@@ -0,0 +1,42 @@
+import nodemailer from 'nodemailer';
+import mandrillTransport from 'nodemailer-mandrill-transport';
+
+import config from '../../config/environment';
+import { Log } from '../../util/log';
+
+export function sendEmail(to, subject, htmlBody, merge_vars, test, metadata) {
+  let apiKey;
+
+  if (test) { apiKey = config.mailSettings.mandrillTestAPIKey; } else { apiKey = config.mailSettings.mandrillAPIKey; }
+
+  if (!apiKey) { return Promise.reject(new Error('No mandrill api key set')); }
+
+  const mailOptions = {};
+  mailOptions.from = config.mailSettings.serverEmail;
+  mailOptions.to = to;
+
+  // Mailing service
+  const mailTransport = nodemailer.createTransport(mandrillTransport({
+    auth: {
+      apiKey,
+    },
+  }));
+
+  mailOptions.mandrillOptions = {
+    template_name: htmlBody,
+    template_content: [],
+    message: {
+      subject,
+      merge: true,
+      merge_language: 'handlebars',
+      global_merge_vars: merge_vars,
+      metadata,
+    },
+  };
+
+  Log.debug('mandrillOptions', mailOptions.mandrillOptions);
+
+  return mailTransport.sendMail(mailOptions);
+}
+
+export default { sendEmail };
