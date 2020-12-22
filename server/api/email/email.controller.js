2568d5f4f59bbf39df9005bec3d474c5e0350e83
paul altefogt <paul@prominentedge.com>
2020-12-21 21:26:17 -0500

    feat(email): pull out partial

--- server/api/email/email.controller.js
+++ server/api/email/email.controller.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/prefer-default-export */
 'use strict';
 
 import moment from 'moment-timezone';
---
+++ server/api/email/emailNotificationFormatter.spec.js
@@ -0,0 +1,125 @@
+import _ from 'lodash';
+import Handlebars from 'handlebars';
+import fs from 'fs';
+import connection from '../../elasticsearch/connection';
+import { IncidentAnalysisTimeRange } from '../../lib/incidentAnalysisTimeRange';
+import { _formatAlerts } from './email.controller';
+import { TimeUnit } from '../../components/constants/time-unit';
+import { InternalServerError } from '../../util/error';
+
+function mockElasticSearch() {
+  connection.getClient = () => {
+    return {
+      msearch: () => {
+        console.log('Hello from elasticsearch mock');
+        return Promise.resolve(elasticSearchMockResponse());
+      },
+    };
+  };
+}
+
+function elasticSearchMockResponse() {
+  const ruleParams = {
+    threshold: 21600, // 6 hours in seconds, default threshold off EventDurationSumRule
+  };
+  const twoIncidentOverThreshold = {
+    aggregations: {
+      apparatus: {
+        'agg_terms_apparatus.unit_id': {
+          buckets: [{
+            'agg_sum_apparatus.extended_data.event_duration':
+              { value: ruleParams.threshold * 2 }, // in seconds
+            key: 'UNIT_100', // unit ID
+            doc_count: 1,
+          },
+          {
+            'agg_sum_apparatus.extended_data.event_duration':
+                { value: ruleParams.threshold * 3 }, // in seconds
+            key: 'UNIT_200', // unit ID
+            doc_count: 4, // 4 units responded
+          },
+          ],
+        },
+      },
+    },
+  };
+  return { responses: [twoIncidentOverThreshold] };
+}
+
+function reportOptions() {
+  const options = { timeUnit: TimeUnit.Day };
+  // Set defautls
+  if (_.isUndefined(options.showPercentChange)) {
+    options.showPercentChange = true;
+  } else {
+    options.showPercentChange = false;
+  }
+
+  if (_.isUndefined(options.showUtilization)) {
+    options.showUtilization = true;
+  } else {
+    options.showUtilization = false;
+  }
+
+  // Override day reports to use shift time.
+  if (options.timeUnit.toLowerCase() === TimeUnit.Day) {
+    options.timeUnit = TimeUnit.Shift;
+  }
+
+  if (_.isNil(options)) {
+    throw new InternalServerError('No report options found!');
+  }
+  console.log('report options', options);
+  return options;
+}
+
+describe('Email Notification Formatter', () => {
+  beforeEach(() => {
+    mockElasticSearch();
+  });
+
+
+  it('should execute without errors', async () => {
+    const analysis = new IncidentAnalysisTimeRange({
+      timeRange: 'test_time_range',
+      index: 'test-index',
+    });
+
+    const ruleAnalysis = await analysis.eventDurationSumRuleAnalysis();
+    console.log('rule analysis', ruleAnalysis);
+
+    const globalMergeVars = [
+      {},
+      {},
+      _formatAlerts(ruleAnalysis, reportOptions()),
+      {},
+      {},
+      {},
+      {},
+      {},
+      {},
+      {},
+    ];
+    console.log('merge vars', JSON.stringify(globalMergeVars));
+
+    const mergeVars = globalMergeVars.slice(0);
+    mergeVars.push({
+      name: 'user',
+      content: {
+        isExternal: 'YES',
+      },
+    });
+    console.log('merge vars', JSON.stringify(mergeVars));
+
+
+    const sourceShell = fs.readFileSync(`${process.cwd()}/server/api/email/templates/shell.hbs`, 'utf-8');
+    const templateShell = Handlebars.compile(sourceShell);
+
+    const sourcePartial = fs.readFileSync(`${process.cwd()}/server/api/email/templates/event.hbs`, 'utf-8');
+    Handlebars.registerPartial('event', sourcePartial);
+
+
+    const html = templateShell(mergeVars);
+    console.log(html);
+  });
+});
