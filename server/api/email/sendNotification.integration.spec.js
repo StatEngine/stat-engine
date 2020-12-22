c3ef9ba2114b82821079d4ba533ace63d353da88
Peter Frollo <peter.frollo@prominentedge.com>
2020-12-22 15:10:23 -0600

    refactor

---
+++ server/api/email/sendNotification.integration.spec.js
@@ -0,0 +1,9 @@
+import { sendNotification } from './sendNotification';
+
+// eslint-disable-next-line no-undef
+describe('sendNotificationEmail()', () => {
+  // eslint-disable-next-line no-undef
+  it('should send a test email', () => {
+    console.log('hello world');
+  });
+});
