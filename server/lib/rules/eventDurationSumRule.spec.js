585640eb4308c1d68fe19caaa7447103095b827e
Peter Frollo <peter.frollo@prominentedge.com>
2020-12-29 09:27:47 -0600

    tlc

--- server/lib/rules/eventDurationSumRule.spec.js
+++ server/lib/rules/eventDurationSumRule.spec.js
@@ -6,27 +6,17 @@ describe('EventDurationSumRule', () => {
   beforeEach(() => {
     const ruleParams = { threshold: 60, // 1 minute in seconds
     };
-    const twoIncidentOverThreshold = {
-      aggregations: {
-        apparatus: {
-          'agg_terms_apparatus.unit_id': {
-            buckets: [{
-              'agg_sum_apparatus.extended_data.event_duration':
-                { value: ruleParams.threshold * 10 }, // in seconds
-              key: 'UNIT_100', // unit ID
-              doc_count: 1,
-            },
-            {
-              'agg_sum_apparatus.extended_data.event_duration':
-                  { value: ruleParams.threshold * 100 }, // in seconds
-              key: 'UNIT_200', // unit ID
-              doc_count: 4, // 4 units responded
-            },
-            ],
-          },
-        },
-      },
+    const incidentOne = {
+      'agg_sum_apparatus.extended_data.event_duration': { value: ruleParams.threshold * 10 }, // in seconds
+      key: 'UNIT_100', // unit ID
+      doc_count: 1,
     };
+    const incidentTwo = {
+      'agg_sum_apparatus.extended_data.event_duration': { value: ruleParams.threshold * 100 }, // in seconds
+      key: 'UNIT_200', // unit ID
+      doc_count: 4, // 4 units responded
+    };
+    const twoIncidentOverThreshold = { aggregations: { apparatus: { 'agg_terms_apparatus.unit_id': { buckets: [incidentOne, incidentTwo] } } } };
     rule = new EventDurationSumRule(ruleParams);
     rule.setResults(twoIncidentOverThreshold);
   });
