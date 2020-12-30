3f7dcf2c19d9fca2f8f96413e35e1a37410f9389
paul altefogt <paul@prominentedge.com>
2020-12-30 12:06:44 -0500

    feat(emails): partial setup

--- server/api/email/sendNotification.integration.spec.js
+++ server/api/email/sendNotification.integration.spec.js
@@ -12,7 +12,7 @@ import config from '../../config/environment';
  */
 
 describe('sendNotification()', () => {
-  it.skip('should send a real email', async () => {
+  it('should send a real email', async () => {
     const htmlReports = new HtmlReports(config.mailSettings.emailTemplatePath);
     const mergeVars = [
       {
