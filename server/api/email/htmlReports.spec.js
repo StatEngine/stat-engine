977c766857f1562ded9fdb0d854f04e1d5f05450
Peter Frollo <peter.frollo@prominentedge.com>
2021-01-05 11:53:42 -0600

    test - extract out expected html report

--- server/api/email/htmlReports.spec.js
+++ server/api/email/htmlReports.spec.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-undef */
 import { expect } from 'chai';
 import handlebars from 'handlebars';
+import fs from 'fs';
 import HtmlReports from './htmlReports';
 import { EventDurationSumRule } from '../../lib/rules/eventDurationSumRule';
 import { _formatAlerts } from './sendNotificationController';
@@ -55,111 +56,10 @@ describe('HtmlReports()', () => {
         handlebars,
         'server/api/email/templates/test/eventDuration/shell.hbs',
         'server/api/email/templates/partials',
-      ).template()).report(globalMergeVars);
+      ).template())
+        .report(globalMergeVars);
 
-      expect(html).to.equal('<div>This is a shell template\n' +
-        '  <div><table border="0" cellpadding="0" cellspacing="0" width="100%" class="mcnBoxedTextBlock" style="min-width:100%;">\n' +
-        '  <!--[if gte mso 9]>\n' +
-        '  <table align="center" border="0" cellspacing="0" cellpadding="0" width="100%">\n' +
-        '  <![endif]-->\n' +
-        '  <tbody class="mcnBoxedTextBlockOuter">\n' +
-        '  <tr>\n' +
-        '    <td valign="top" class="mcnBoxedTextBlockInner">\n' +
-        '      <!--[if gte mso 9]>\n' +
-        '      <td align="center" valign="top" ">\n' +
-        '      <![endif]-->\n' +
-        '      <table align="left" border="0" cellpadding="0" cellspacing="0" width="100%" style="min-width:100%;"\n' +
-        '             class="mcnBoxedTextContentContainer">\n' +
-        '        <tbody>\n' +
-        '        <tr>\n' +
-        '          <td style="padding-top:9px; padding-left:18px; padding-bottom:9px; padding-right:18px;">\n' +
-        '            <table border="0" cellspacing="0" class="mcnTextContentContainer" width="100%"\n' +
-        '                   style="min-width: 100% !important;background-color: #616161;">\n' +
-        '              <tbody>\n' +
-        '              <tr>\n' +
-        '                <td valign="top" class="mcnTextContent"\n' +
-        '                    style="padding: 18px;color: #F2F2F2;font-family: Helvetica;font-size: 24px;font-weight: normal;text-align: center;">\n' +
-        '                  <div style="text-align: center;"><strong>Notifications</strong></div>\n' +
-        '                </td>\n' +
-        '              </tr>\n' +
-        '              </tbody>\n' +
-        '            </table>\n' +
-        '          </td>\n' +
-        '        </tr>\n' +
-        '        </tbody>\n' +
-        '      </table>\n' +
-        '      <!--[if gte mso 9]>\n' +
-        '      </td>\n' +
-        '      <![endif]-->\n' +
-        '      <!--[if gte mso 9]>\n' +
-        '      </tr>\n' +
-        '      </table>\n' +
-        '      <![endif]-->\n' +
-        '    </td>\n' +
-        '  </tr>\n' +
-        '  </tbody>\n' +
-        '</table>\n' +
-        '<table border="0" cellpadding="0" cellspacing="0" width="100%" class="mcnTextBlock" style="min-width:100%;">\n' +
-        '  <tbody class="mcnTextBlockOuter">\n' +
-        '  <tr>\n' +
-        '    <td valign="top" class="mcnTextBlockInner" style="padding-top:9px;">\n' +
-        '      <!--[if mso]>\n' +
-        '      <table align="left" border="0" cellspacing="0" cellpadding="0" width="100%" style="width:100%;">\n' +
-        '        <tr>\n' +
-        '      <![endif]-->\n' +
-        '      <!--[if mso]>\n' +
-        '      <td valign="top" width="598" style="width:598px;">\n' +
-        '      <![endif]-->\n' +
-        '      <table align="left" border="0" cellpadding="0" cellspacing="0" style="max-width:100%; min-width:100%;"\n' +
-        '             width="100%" class="mcnTextContentContainer">\n' +
-        '        <tbody>\n' +
-        '        <tr>\n' +
-        '          <td valign="top" class="mcnTextContent"\n' +
-        '              style="padding-top:0; padding-right:18px; padding-bottom:9px; padding-left:18px;">\n' +
-        '            <table style="width:100%">\n' +
-        '              <tbody>\n' +
-        '              <tr>\n' +
-        '                <th style="text-align: left;"><span style="font-size:16px"><span style="color:#000000"><strong>Description</strong></span></span>\n' +
-        '                </th>\n' +
-        '                <th style="text-align: left;"><span style="font-size:14px"><span\n' +
-        '                        style="color:#000000"><strong>Details</strong></span></span>\n' +
-        '                </th>\n' +
-        '              </tr>\n' +
-        '                <tr style="background-color: #f2dede">\n' +
-        '                  <td>\n' +
-        '                    <span style="font-size:14px;">Unit utilization &gt; 1 min</span>\n' +
-        '                  </td>\n' +
-        '                  <td>\n' +
-        '                    <span style="font-size:14px;">Unit: UNIT_100, Utilization: 10.00</span>\n' +
-        '                  </td>\n' +
-        '                </tr>\n' +
-        '                <tr style="background-color: #f2dede">\n' +
-        '                  <td>\n' +
-        '                    <span style="font-size:14px;">Unit utilization &gt; 1 min</span>\n' +
-        '                  </td>\n' +
-        '                  <td>\n' +
-        '                    <span style="font-size:14px;">Unit: UNIT_200, Utilization: 100.00</span>\n' +
-        '                  </td>\n' +
-        '                </tr>\n' +
-        '              </tbody>\n' +
-        '            </table>\n' +
-        '          </td>\n' +
-        '        </tr>\n' +
-        '        </tbody>\n' +
-        '      </table>\n' +
-        '      <!--[if mso]>\n' +
-        '      </td>\n' +
-        '      <![endif]-->\n' +
-        '      <!--[if mso]>\n' +
-        '      </tr>\n' +
-        '      </table>\n' +
-        '      <![endif]-->\n' +
-        '    </td>\n' +
-        '  </tr>\n' +
-        '  </tbody>\n' +
-        '</table>\n' +
-        '</div>\n' +
-        '</div>\n');
+      expect(html).to.equal(fs.readFileSync('server/api/email/test/data/EventDurationHtmlReport.html', 'utf-8'));
     });
   });
 });
