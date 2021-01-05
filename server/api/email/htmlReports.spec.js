a4986f9b6d7872927223752406d48091f31f247b
Peter Frollo <peter.frollo@prominentedge.com>
2021-01-05 11:53:42 -0600

    test - extract out expected html report

--- server/api/email/htmlReports.spec.js
+++ server/api/email/htmlReports.spec.js
@@ -70,80 +70,14 @@ describe('HtmlReports', () => {
         eventDurationSumRuleWithData = new EventDurationSumRule(ruleParams);
         eventDurationSumRuleWithData.setResults(incidents);
 
-        htmlReports = new HtmlReports(new HandlebarsEmailTemplate(
-          handlebars,
-          'server/api/email/templates/test/eventDuration/shell.hbs',
-          'server/api/email/templates/partials',
-        ).template());
-      });
-      it('should generate Event Duration html report', () => {
-        const data = toData(eventDurationSumRuleWithData);
-        const html = htmlReports.report(data);
-        expect(html).to.equal(fs.readFileSync('server/api/email/test/data/EventDurationHtmlReportV2.html', 'utf-8'));
-      });
-    });
-    describe('Test HtmlReports & HandlebarsEmailTemplate', () => {
-      it('should create condensed Email Duration Report', () => {
-        const data =
-          [
-            {
-              name: 'alerts',
-              content: [
-                {
-                  rule: 'EventDurationSumRule',
-                  level: 'DANGER',
-                  description: 'Unit utilization > 1 min',
-                  detailList: [
-                    { detail: 'UNIT_000/100.00' },
-                    { detail: 'UNIT_100/200.00' },
-                    { detail: 'UNIT_200/300.00' },
-                    { detail: 'UNIT_300/400.00' },
-                    { detail: 'UNIT_400/500.00' },
-                  ],
-                  default_visibility: true,
-                  rowColor: '#f2dede',
-                  rowBorderColor: '#bb7474',
-                },
-                {
-                  rule: 'EventDurationSumRule',
-                  level: 'DANGER',
-                  description: 'Unit utilization > 1 min',
-                  detailList: [
-                    { detail: 'UNIT_1000/100.00' },
-                    { detail: 'UNIT_1100/200.00' },
-                    { detail: 'UNIT_1200/300.00' },
-                    { detail: 'UNIT_1300/400.00' },
-                    { detail: 'UNIT_1400/500.00' },
-                  ],
-                  default_visibility: true,
-                  rowColor: '#f2dede',
-                  rowBorderColor: '#bb7474',
-                },
-                {
-                  rule: 'EventDurationSumRule',
-                  level: 'DANGER',
-                  description: 'Unit utilization > 1 min',
-                  detailList: [
-                    { detail: 'UNIT_2000/100.00' },
-                    { detail: 'UNIT_2100/200.00' },
-                    { detail: 'UNIT_2200/300.00' },
-                    { detail: 'UNIT_2300/400.00' },
-                  ],
-                  default_visibility: true,
-                  rowColor: '#f2dede',
-                  rowBorderColor: '#bb7474',
-                },
-              ],
-            },
-          ];
-        const htmlReport = new HtmlReports(new HandlebarsEmailTemplate(
-          handlebars,
-          'server/api/email/templates/test/eventDuration/shell.hbs',
-          'server/api/email/templates/partials',
-        ).template());
-        const html = htmlReport.report(data);
-        expect(html).to.equal(fs.readFileSync('server/api/email/test/data/EventDurationHtmlReport.html', 'utf-8'));
-      });
+      const html = new HtmlReports(new HandlebarsEmailTemplate(
+        handlebars,
+        'server/api/email/templates/test/eventDuration/shell.hbs',
+        'server/api/email/templates/partials',
+      ).template())
+        .report(globalMergeVars);
+
+      expect(html).to.equal(fs.readFileSync('server/api/email/test/data/EventDurationHtmlReport.html', 'utf-8'));
     });
   });
 });
