31bbaeb597c65d4b708cc702d216d10d290e42ea
Peter Frollo <peter.frollo@prominentedge.com>
2021-01-15 14:03:49 -0600

    tlc

--- server/lib/rules/overnightEventsRule.js
+++ server/lib/rules/overnightEventsRule.js
@@ -79,16 +79,25 @@ export class OvernightEventsRule extends Rule {
     };
   }
 
-  finalReportDetails(reportDetails, params) {
+  /**
+   * Creates a report by condensing of the original report. It creates chunks or arrays of details.
+   * The final report is enriched with fields from the params parameter
+   *
+   * @param report report to be condensed and enriched
+   * @param reportChunkSize determines how many reports are condensed to a single report
+   * @param enticements static values used to enrich the report
+   * @returns {[]}
+   */
+  finalReportDetails(report, reportChunkSize, enticements) {
     const analysis = [];
 
-    _.chunk(reportDetails, params.reportChunkSize)
+    _.chunk(report, enticements.reportChunkSize)
       .forEach(detail => {
         analysis.push({
-          rule: params.reportRuleName,
-          level: params.reportLevel,
-          description: params.reportDescription,
-          default_visibility: params.reportDefaultVisibility,
+          rule: enticements.reportRuleName,
+          level: enticements.reportLevel,
+          description: enticements.reportDescription,
+          default_visibility: enticements.reportDefaultVisibility,
           detailList: detail,
         });
       });
