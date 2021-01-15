98085371517f6caeba7c7ef5d595b8dd5a4044cf
Peter Frollo <peter.frollo@prominentedge.com>
2021-01-15 11:21:07 -0600

    modify OvernightEventsRule to generate data using the condensed data format

--- server/lib/rules/overnightEventsRule.js
+++ server/lib/rules/overnightEventsRule.js
@@ -5,11 +5,29 @@ import { Rule } from '../rule';
 export class OvernightEventsRule extends Rule {
   constructor(params) {
     super(params);
-    this.params.threshold = this.params.threshold || 7200;
-    this.params.level = 'DANGER';
+    this.level = 'DANGER';
+    this.ruleName = this.constructor.name;
     this.reportChunkSize = 5;
+    this.defaultVisibility = true;
     this.unitRespondingCountThreshold = 2;
+    this.unitUtilTimeThreshold = this.params.threshold || 7200;
 
+
+    this.utilizationMinutesReportParams = {
+      reportLevel: this.level,
+      reportRuleName: this.ruleName,
+      reportChunkSize: this.reportChunkSize,
+      reportDefaultVisibility: this.defaultVisibility,
+      reportDescription: `Unit utilization > ${(this.params.threshold / 60.0).toFixed(0)} min overnight`,
+    };
+
+    this.utilizationCountReportParms = {
+      reportLevel: this.level,
+      reportRuleName: this.ruleName,
+      reportChunkSize: this.reportChunkSize,
+      reportDefaultVisibility: this.defaultVisibility,
+      reportDescription: 'Unit response > 2 overnight',
+    };
 
     this.query = bodybuilder()
       .filter('term', 'description.suppressed', false)
@@ -20,54 +38,60 @@ export class OvernightEventsRule extends Rule {
           .aggregation('sum', 'apparatus.extended_data.event_duration')));
   }
 
+  /**
+   * Performs two analyses, unit time utilization and number of units utilized
+   *
+   * @returns {*[]} unit utilization time and unit utilization count report
+   */
   analyze() {
-    // TODO move the constant to the constructor
-    // eslint-disable-next-line camelcase
-    const default_visibility = true;
-    const rule = this.constructor.name;
-    const level = this.params.level;
-    const analysis = [];
-    const detailsUtilizationMinutes = [];
-    const detailsUtilizationCount = [];
+    const { utilMinReportDetails, utilCountReportDetails } =
+      this.reportDetails(this.results, this.unitUtilTimeThreshold, this.unitRespondingCountThreshold);
+    const utilMinReport = this.finalReportDetails(utilMinReportDetails, this.utilizationMinutesReportParams);
+    const utilCountReport = this.finalReportDetails(utilCountReportDetails, this.utilizationCountReportParms);
+    return utilMinReport.concat(utilCountReport);
+  }
 
-    this.results.aggregations.apparatus['agg_terms_apparatus.unit_id'].buckets.forEach(unit => {
+  /**
+   * Performs two analyses using provided threshold, unit time utilization over the threshold
+   *  and number of units utilized over the threshold
+   * @param utilData data to be analyzed
+   * @param utilMinThreshold utilization time threshold
+   * @param utilCountThreshold utilization count threshold
+   * @returns {{utilMinReportDetails: [], utilCountReportDetails: []}} unit utilization time and unit utilization count report
+   */
+  reportDetails(utilData, utilMinThreshold, utilCountThreshold) {
+    const utilMinReportDetails = [];
+    const utilCountReportDetails = [];
+
+    utilData.aggregations.apparatus['agg_terms_apparatus.unit_id'].buckets.forEach(unit => {
       const utilization = unit['agg_sum_apparatus.extended_data.event_duration'].value;
-
       const count = unit.doc_count;
-
-      if (utilization > this.params.threshold) {
-        detailsUtilizationMinutes.push({ detail: `${unit.key}/${(utilization / 60.0).toFixed(2)}` });
+      if (utilization > utilMinThreshold) {
+        utilMinReportDetails.push({ detail: `${unit.key}/${(utilization / 60.0).toFixed(2)}` });
       }
-
-      if (count > this.unitRespondingCountThreshold) {
-        detailsUtilizationCount.push({ detail: `${unit.key}/${count}` });
+      if (count > utilCountThreshold) {
+        utilCountReportDetails.push({ detail: `${unit.key}/${count}` });
       }
     });
+    return {
+      utilMinReportDetails,
+      utilCountReportDetails,
+    };
+  }
 
-    // TODO reuse
-    let description = `Unit utilization > ${(this.params.threshold / 60.0).toFixed(0)} min overnight`;
-    _.chunk(detailsUtilizationMinutes, this.reportChunkSize).forEach(detail => {
-      analysis.push({
-        rule,
-        level,
-        description,
-        default_visibility,
-        detailList: detail,
+  finalReportDetails(reportDetails, params) {
+    const analysis = [];
+
+    _.chunk(reportDetails, params.reportChunkSize)
+      .forEach(detail => {
+        analysis.push({
+          rule: params.reportRuleName,
+          level: params.reportLevel,
+          description: params.reportDescription,
+          default_visibility: params.reportDefaultVisibility,
+          detailList: detail,
+        });
       });
-    });
-
-    // TODO reuse
-    description = 'Unit response > 2 overnight';
-    _.chunk(detailsUtilizationCount, this.reportChunkSize).forEach(detail => {
-      analysis.push({
-        rule,
-        level,
-        description,
-        default_visibility,
-        detailList: detail,
-      });
-    });
-
     return analysis;
   }
 }
