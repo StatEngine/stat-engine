655392f96f0d5bed4eb03b0e951e527950dd79f7
paul altefogt <paul@prominentedge.com>
2021-02-01 12:33:07 -0500

    test(emails): add tests for remaining comparison metrics

--- server/lib/emails/formatAggregateMetrics.js
+++ server/lib/emails/formatAggregateMetrics.js
@@ -1,17 +1,11 @@
 import _ from 'lodash';
 
 export default function formatAggregateMetrics(key, metricConfigs, comparison, options) {
-  // console.log('formatAggregateMetrics');
-  // console.log(key);
-  // console.dir(comparison, { depth: null });
-
   const keyName = `${key}Metrics`;
   const mergeVar = initMergeVar(`${key}Summary`, keyName);
 
   _.forEach(comparison[key], (metrics, id) => {
     const obj = { id };
-    // console.log('obj');
-    // console.dir(obj);
     metricConfigs.forEach(metricConfig => {
       const [path, condition] = metricConfig;
 
@@ -28,7 +22,6 @@ export default function formatAggregateMetrics(key, metricConfigs, comparison, o
   mergeVar[keyName] = _.sortBy(mergeVar[keyName], [o => _.get(o, 'incidentCount.val')]).reverse();
   mergeVar[keyName] = _.filter(mergeVar[keyName], o => _.get(o, 'incidentCount.val') !== 0);
 
-  // console.dir(mergeVar, { depth: null });
   return mergeVar;
 }
 
--- test/server/lib/emails/sections/comparisonMetricSummary.spec.js
+++ test/server/lib/emails/sections/comparisonMetricSummary.spec.js
@@ -6,34 +6,34 @@ import comparisonMetricSummary from '../../../../../server/lib/emails/sections/c
 describe('comparisonMetricSummary', () => {
   describe('fundamental tests', () => {
     describe('error path tests', () => {
-      it.skip('should throw an exception when comparison is missing', () => {
+      it('should throw an exception when comparison is missing', () => {
         const params = { reportOptions: { foo: 'bar' } };
         const type = 'agencyIncidentType';
         expect(() => comparisonMetricSummary(type, params)).to.throw('Missing comparison or reportOptions');
       });
-      it.skip('should throw an exception when comparison is empty', () => {
+      it('should throw an exception when comparison is empty', () => {
         const params = { comparison: {}, reportOptions: { foo: 'bar' } };
         const type = 'agencyIncidentType';
         expect(() => comparisonMetricSummary(type, params)).to.throw('Missing comparison or reportOptions');
       });
-      it.skip('should throw an exception when reportOptions is missing', () => {
+      it('should throw an exception when reportOptions is missing', () => {
         const params = { comparison: { foo: 'bar' } };
         const type = 'agencyIncidentType';
         expect(() => comparisonMetricSummary(type, params)).to.throw('Missing comparison or reportOptions');
       });
-      it.skip('should throw an exception when reportOptions is empty', () => {
+      it('should throw an exception when reportOptions is empty', () => {
         const params = { comparison: { foo: 'bar' }, reportOptions: {} };
         const type = 'agencyIncidentType';
         expect(() => comparisonMetricSummary(type, params)).to.throw('Missing comparison or reportOptions');
       });
-      it.skip('should throw an exception when type is unsupported', () => {
+      it('should throw an exception when type is unsupported', () => {
         const params = { comparison: { foo: 'bar' }, reportOptions: { yar: 'barlog' } };
         const type = 'baz';
         expect(() => comparisonMetricSummary(type, params)).to.throw('No metric config for metric type: baz');
       });
     });
     describe('happy path tests', () => {
-      it('should create a valid mergeVar for agencyIncidentType', async () => {
+      it('should create a valid mergeVar for agencyIncidentType', () => {
         const mockDataPath = 'test/server/lib/emails/sections/mocks';
         const mockOptionsFileName = 'agencyIncidentTypeSummaryOptions.mock.json';
         const mockComparisonFileName = 'comparison.mock.json';
@@ -50,117 +50,103 @@ describe('comparisonMetricSummary', () => {
           agencyIncidentTypeMetrics: [],
         };
 
-        const mergeVar = await comparisonMetricSummary('agencyIncidentType', params);
+        const mergeVar = comparisonMetricSummary('agencyIncidentType', params);
 
         expect(mergeVar).to.deep.equal(expected);
       });
 
-      it.skip('should create a valid mergeVar for agencyResponses', async () => {
+      it('should create a valid mergeVar for agencyResponses', () => {
         const mockDataPath = 'test/server/lib/emails/sections/mocks';
         const mockOptionsFileName = 'agencyResponsesSummaryOptions.mock.json';
         const mockComparisonFileName = 'comparison.mock.json';
+        const mockAgencyResponsesMergeVar = 'agencyResponsesSummaryMergeVar.mock.json';
         const reportOptions = loadJson(mockDataPath, mockOptionsFileName);
         const comparison = loadJson(mockDataPath, mockComparisonFileName);
+        const expected = loadJson(mockDataPath, mockAgencyResponsesMergeVar);
 
         const params = {
           comparison,
           reportOptions,
         };
 
-        const expected = {
-          name: 'agencyResponsesSummary',
-          agencyResponsesMetrics: [],
-        };
-
-        const mergeVar = await comparisonMetricSummary('agencyResponses', params);
+        const mergeVar = comparisonMetricSummary('agencyResponses', params);
 
         expect(mergeVar).to.deep.equal(expected);
       });
 
-      it.skip('should create a valid mergeVar for battalion', async () => {
+      it('should create a valid mergeVar for battalion', () => {
         const mockDataPath = 'test/server/lib/emails/sections/mocks';
         const mockOptionsFileName = 'battalionSummaryOptions.mock.json';
         const mockComparisonFileName = 'comparison.mock.json';
+        const mockBattalionSummaryMergeVarFileName = 'battalionSummaryMergeVar.mock.json';
         const reportOptions = loadJson(mockDataPath, mockOptionsFileName);
         const comparison = loadJson(mockDataPath, mockComparisonFileName);
+        const expected = loadJson(mockDataPath, mockBattalionSummaryMergeVarFileName);
 
         const params = {
           comparison,
           reportOptions,
         };
 
-        const expected = {
-          name: 'battalionSummary',
-          battalionMetrics: [],
-        };
-
-        const mergeVar = await comparisonMetricSummary('battalion', params);
+        const mergeVar = comparisonMetricSummary('battalion', params);
 
         expect(mergeVar).to.deep.equal(expected);
       });
 
-      it.skip('should create a valid mergeVar for incidentType', async () => {
+      it('should create a valid mergeVar for incidentType', () => {
         const mockDataPath = 'test/server/lib/emails/sections/mocks';
         const mockOptionsFileName = 'incidentTypeSummaryOptions.mock.json';
         const mockComparisonFileName = 'comparison.mock.json';
+        const mockIncidentTypeSumaryMergeVarFileName = 'incidentTypeSummaryMergeVar.mock.json';
+
         const reportOptions = loadJson(mockDataPath, mockOptionsFileName);
         const comparison = loadJson(mockDataPath, mockComparisonFileName);
+        const expected = loadJson(mockDataPath, mockIncidentTypeSumaryMergeVarFileName);
 
         const params = {
           comparison,
           reportOptions,
         };
 
-        const expected = {
-          name: 'incidentTypeSummary',
-          incidentTypeMetrics: [],
-        };
-
-        const mergeVar = await comparisonMetricSummary('incidentType', params);
+        const mergeVar = comparisonMetricSummary('incidentType', params);
 
         expect(mergeVar).to.deep.equal(expected);
       });
 
-      it.skip('should create a valid mergeVar for jurisdiction', async () => {
+      it('should create a valid mergeVar for jurisdiction', () => {
         const mockDataPath = 'test/server/lib/emails/sections/mocks';
         const mockOptionsFileName = 'jurisdictionSummaryOptions.mock.json';
         const mockComparisonFileName = 'comparison.mock.json';
+        const mockJurisdictionSummaryMergeVarFileName = 'jurisdictionSummaryMergeVar.mock.json';
         const reportOptions = loadJson(mockDataPath, mockOptionsFileName);
         const comparison = loadJson(mockDataPath, mockComparisonFileName);
+        const expected = loadJson(mockDataPath, mockJurisdictionSummaryMergeVarFileName);
 
         const params = {
           comparison,
           reportOptions,
         };
 
-        const expected = {
-          name: 'jurisdictionSummary',
-          jurisdictionMetrics: [],
-        };
-
-        const mergeVar = await comparisonMetricSummary('jurisdiction', params);
+        const mergeVar = comparisonMetricSummary('jurisdiction', params);
 
         expect(mergeVar).to.deep.equal(expected);
       });
 
-      it.skip('should create a valid mergeVar for unit', async () => {
+      it('should create a valid mergeVar for unit', () => {
         const mockDataPath = 'test/server/lib/emails/sections/mocks';
         const mockOptionsFileName = 'unitSummaryOptions.mock.json';
         const mockComparisonFileName = 'comparison.mock.json';
+        const mockUnitSummaryMergeVarFileName = 'unitSummaryMergeVar.mock.json';
         const reportOptions = loadJson(mockDataPath, mockOptionsFileName);
         const comparison = loadJson(mockDataPath, mockComparisonFileName);
+        const expected = loadJson(mockDataPath, mockUnitSummaryMergeVarFileName);
 
         const params = {
           comparison,
           reportOptions,
         };
 
-        const expected = {
-          name: 'unitSummary',
-          unitMetrics: [],
-        };
-
-        const mergeVar = await comparisonMetricSummary('unit', params);
+        const mergeVar = comparisonMetricSummary('unit', params);
 
         expect(mergeVar).to.deep.equal(expected);
       });
