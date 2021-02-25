ea5e81de7d888893b44999b3e05d72770546a17a
paul altefogt <paul@prominentedge.com>
2021-02-24 21:46:39 -0500

    test(emails): just for testing

--- server/lib/emails/notificationEmails.js
+++ server/lib/emails/notificationEmails.js
@@ -7,14 +7,15 @@ import getEmailHtml from '../../api/email/getEmailHtmlController';
 
 export default async function handleNotificationEmail(emailConfigId, startDate, endDate, previous, fireDepartment, test) {
   // TESTING reportOptions is for testing only
+  // richmond
   // const reportOptions = {
   //   name: 'Daily',
   //   timeUnit: 'SHIFT',
   //   sections: {
   //     showAlertSummary: { FireIncidentEventDurationRule30: false },
   //     showBattalionSummary: true,
-  //     showIncidentTypeSummary: true,
-  //     showAgencyIncidentTypeSummary: true,
+  //     showIncidentTypeSummary: false,
+  //     showAgencyIncidentTypeSummary: false,
   //     showUnitAgencySummary: true,
   //   },
   //   showDistances: false,
@@ -28,89 +29,90 @@ export default async function handleNotificationEmail(emailConfigId, startDate, 
   //   schedulerOptions: { later: { text: 'every 2 minutes' } },
   // };
 
-  // const reportOptions = {
-  //   name: 'Daily',
-  //   timeUnit: 'SHIFT',
-  //   sections: {
-  //     showAlertSummary: true,
-  //     showBattalionSummary: false,
-  //     showIncidentTypeSummary: true,
-  //     showAgencyIncidentTypeSummary: false,
-  //     showUnitAgencySummary: true,
-  //     showJurisdictionSummary: true,
-  //   },
-  //   emailAllUsers: true,
-  //   showDistances: false,
-  //   showTransports: true,
-  //   showPercentChange: true,
-  //   showUtilization: true,
-  //   logo: 'https://s3.amazonaws.com/statengine-public-assets/logos/93429.png',
-  //   schedulerOptions: { later: { text: 'every 2 minutes' } },
-  // };
-
-  // roseville
+  // rincon
   const reportOptions = {
     name: 'Daily',
     timeUnit: 'SHIFT',
     sections: {
-      showAlertSummary: { FireIncidentEventDurationRule30: false },
+      showAlertSummary: true,
+      showBattalionSummary: false,
       showIncidentTypeSummary: true,
       showAgencyIncidentTypeSummary: false,
       showUnitAgencySummary: true,
-      showBattalionSummary: false,
       showJurisdictionSummary: true,
     },
-    showPercentChange: false,
+    emailAllUsers: true,
     showDistances: false,
-    showTransports: false,
+    showTransports: true,
+    showPercentChange: true,
     showUtilization: true,
-    previous: false,
-    logo: 'https://s3.amazonaws.com/statengine-public-assets/logos/77855.png',
-    schedulerOptions: { later: { text: 'at 1:05 pm' } },
+    logo: 'https://s3.amazonaws.com/statengine-public-assets/logos/93429.png',
+    schedulerOptions: { later: { text: 'every 2 minutes' } },
   };
 
-  // rincon valley
-  // fireDepartment = {
-  //   es_indices: {
-  //     all: '93429-az-rincon_valley_fire_district*',
-  //     'fire-incident': '93429-az-rincon_valley_fire_district-fire-incident*',
-  //     'apparatus-fire-incident': '93429-az-rincon_valley_fire_district-apparatus-fire-incident*',
-  //     'vehicle-telemetry': '93429-az-rincon_valley_fire_district-vehicle-telemetry*',
+  // roseville
+  // const reportOptions = {
+  //   name: 'Daily',
+  //   timeUnit: 'SHIFT',
+  //   sections: {
+  //     showAlertSummary: { FireIncidentEventDurationRule30: false },
+  //     showIncidentTypeSummary: true,
+  //     showAgencyIncidentTypeSummary: false,
+  //     showUnitAgencySummary: true,
+  //     showBattalionSummary: false,
+  //     showJurisdictionSummary: true,
   //   },
-  //   _id: 72,
-  //   fd_id: '11152',
-  //   name: 'Rincon Valley Fire District',
-  //   state: 'AZ',
-  //   firecares_id: '93429',
-  //   timezone: 'US/Arizona',
-  //   integration_complete: true,
-  //   integration_verified: true,
-  //   latitude: 32.0862,
-  //   longitude: -110.712,
-  //   logo_link: 'https://s3.amazonaws.com/statengine-public-assets/logos/93429.png',
-  //   customer_id: 'AzZcotRumF5B9Ajl',
+  //   showPercentChange: false,
+  //   showDistances: false,
+  //   showTransports: false,
+  //   showUtilization: true,
+  //   previous: false,
+  //   logo: 'https://s3.amazonaws.com/statengine-public-assets/logos/77855.png',
+  //   schedulerOptions: { later: { text: 'at 1:05 pm' } },
   // };
 
-  // roseville
+  // rincon valley
   fireDepartment = {
     es_indices: {
-      all: '77855-ca-city_of_roseville_fire_department*',
-      'fire-incident': '77855-ca-city_of_roseville_fire_department-fire-incident*',
-      'apparatus-fire-incident': '77855-ca-city_of_roseville_fire_department-apparatus-fire-incident*',
+      all: '93429-az-rincon_valley_fire_district*',
+      'fire-incident': '93429-az-rincon_valley_fire_district-fire-incident*',
+      'apparatus-fire-incident': '93429-az-rincon_valley_fire_district-apparatus-fire-incident*',
+      'vehicle-telemetry': '93429-az-rincon_valley_fire_district-vehicle-telemetry*',
     },
     _id: 72,
     fd_id: '11152',
-    name: 'City of Roseville Fire Department',
-    state: 'CA',
-    firecares_id: '77855',
-    timezone: 'US/Pacific',
+    name: 'Rincon Valley Fire District',
+    state: 'AZ',
+    firecares_id: '93429',
+    timezone: 'US/Arizona',
     integration_complete: true,
     integration_verified: true,
     latitude: 32.0862,
     longitude: -110.712,
-    logo_link: 'https://s3.amazonaws.com/statengine-public-assets/logos/77855.png',
+    logo_link: 'https://s3.amazonaws.com/statengine-public-assets/logos/93429.png',
     customer_id: 'AzZcotRumF5B9Ajl',
   };
+
+  // roseville
+  // fireDepartment = {
+  //   es_indices: {
+  //     all: '77855-ca-city_of_roseville_fire_department*',
+  //     'fire-incident': '77855-ca-city_of_roseville_fire_department-fire-incident*',
+  //     'apparatus-fire-incident': '77855-ca-city_of_roseville_fire_department-apparatus-fire-incident*',
+  //   },
+  //   _id: 72,
+  //   fd_id: '11152',
+  //   name: 'City of Roseville Fire Department',
+  //   state: 'CA',
+  //   firecares_id: '77855',
+  //   timezone: 'US/Pacific',
+  //   integration_complete: true,
+  //   integration_verified: true,
+  //   latitude: 32.0862,
+  //   longitude: -110.712,
+  //   logo_link: 'https://s3.amazonaws.com/statengine-public-assets/logos/77855.png',
+  //   customer_id: 'AzZcotRumF5B9Ajl',
+  // };
 
 
   // COMMENTED OUT FOR TESTING const reportOptions = await getReportOptions(emailConfigId, fireDepartment._id);
